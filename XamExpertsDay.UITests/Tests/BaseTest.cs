03ab5b178023a2b3a550f6b9e74ade88adeb2161
Stefanija <stefanija__popovska@hotmail.com>
2019-10-06 10:59:28 +0200

    Add tests to souluton

---
+++ XamExpertsDay.UITests/Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace XamExpertsDay.UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+
+    public abstract class BaseTest
+    {
+        protected IApp app;
+        protected Platform platform;
+
+        protected BaseTest(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        virtual public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            app.Screenshot("App Initialized");
+        }
+    }
+}
