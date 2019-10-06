0b67f36e4672e2e60178627fa62da7a639396d73
Stefanija <stefanija__popovska@hotmail.com>
2019-10-06 11:00:52 +0200

    add app tests

--- XamExpertsDay.UITests/Tests/MockDataTests.cs
+++ XamExpertsDay.UITests/Tests/MockDataTests.cs
@@ -12,33 +12,17 @@ namespace XamExpertsDay.UITests
         public override void BeforeEachTest()
         {
             base.BeforeEachTest();
-
-            ItemsPage.WaitForPageToLoad();
         }
 
         [Test]
         public void AddNewItem()
         {
-            //Arrange
-            const string itemName = "Item Name";
-            const string itemDescription = "Item Description";
-
-            //Act
-            ItemsPage.TapAddToolbarButton();
-            NewItemPage.EnterItemName(itemName);
-            NewItemPage.EnterItemDescription(itemDescription);
-            NewItemPage.TapSaveToolbarButton();
-
-            //Assert
-            Assert.IsTrue(ItemsPage.IsPageVisible);
-            Assert.IsTrue(app.Query(itemName).Length > 0);
-        }
-
-        [Ignore("")]
-        [Test]
-        public void Repl()
-        {
-            app.Repl();
+            app.Tap(x => x.Marked("Agenda"));
+            app.Screenshot("Navigate to agenda tab");
+            app.Tap(x => x.Marked("About"));
+            app.Screenshot("App navigated back");
+            app.Tap(x => x.Marked("Learn more"));
+            app.Screenshot("Learn More Button Clicked");
         }
     }
 }
